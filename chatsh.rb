c8a46c47276f4dd0e708ff56c1b0630e402f6a9c
Yi-Wei Chang <31400914+jchang6513@users.noreply.github.com>
2026-03-21 12:39:10 +0800

    Update to v0.1.2

--- chatsh.rb
+++ chatsh.rb
@@ -1,11 +1,16 @@
 cask "chatsh" do
-  version "0.1.0"
-  sha256 "7161d55c3e6e954430dd371e98d9de4eb408bb90a9f6c4057a5e67f305801131"
+  version "0.1.2"
+  sha256 "7a32aa8ef7dbf8aed0a6d41cb01682e3b38d33572a556070fc530474e3cc9504"
 
   url "https://github.com/jchang6513/chatsh/releases/download/v#{version}/chatsh_#{version}_aarch64.dmg"
   name "chat.sh"
-  desc "Terminal-native desktop app for managing multiple AI coding assistants"
-  homepage "https://github.com/jchang6513/chatsh"
+  desc "Terminal-native desktop app for managing multiple AI REPL sessions"
+  homepage "https://chatsh-terminal.vercel.app"
 
   app "chat.sh.app"
+
+  zap trash: [
+    "~/.chatsh",
+    "~/Library/WebKit/chatsh",
+  ]
 end
